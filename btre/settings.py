d7af42cff89c2d94bb0ec1f5d9833eb5714b05b2
fzhu47 <zhufeng0831@gmail.com>
2020-06-24 16:33:07 -0400

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'zhufeng0831@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
